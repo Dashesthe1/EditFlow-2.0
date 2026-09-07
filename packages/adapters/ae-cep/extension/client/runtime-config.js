f2a31a2304fb71ca806a2dc8d838fb1da3e169a0
Dashesthe1 <olufemioluwaseun777@gmail.com>
2026-09-06 22:57:01 -0500

    Advertise protocol 1.4 in checked-in CEP config

--- packages/adapters/ae-cep/extension/client/runtime-config.js
+++ packages/adapters/ae-cep/extension/client/runtime-config.js
@@ -5,7 +5,7 @@ window.EDITFLOW2_BRIDGE_CONFIG = Object.freeze({
   port: 0,
   token: "",
   protocolVersion: "1.1.0",
-  supportedProtocolVersions: ["1.3.0", "1.2.0", "1.1.0"],
+  supportedProtocolVersions: ["1.4.0", "1.3.0", "1.2.0", "1.1.0"],
   extensionId: "com.editflow2.bridge.panel",
   extensionVersion: "0.1.0-dev.4"
 });
