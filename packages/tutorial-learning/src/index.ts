cc39d23b21faa1ca8f920e43ae95f872b266709d
Dashesthe1 <olufemioluwaseun777@gmail.com>
2026-09-17 20:36:00 -0500

    Select tutorial proofs by cache and impact

--- packages/tutorial-learning/src/index.ts
+++ packages/tutorial-learning/src/index.ts
@@ -4,4 +4,5 @@ export * from "./capability-discovery.js";
 export * from "./pipeline.js";
 export * from "./deep-analysis.js";
 export * from "./proof-compiler.js";
+export * from "./proof-selector.js";
 export * from "./deep-pipeline.js";
